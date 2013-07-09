c99dfd2dbe9241240cb5c532ec88e89957e4aadd
Johanbo123 <johanbo@sim.sanlam.com>
2013-07-09 21:23:49 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,51 @@
+#!/usr/bin/env node
+"use strict";
+var fs = require("fs");
 var express = require('express');
+var app = express();
+var fileName = "index.html";
+var buffer, buf_len, data;
+var fileRead = false;
 
-var app = express.createServer(express.logger());
+var Close = function(err){
+    if (err) { throw err; };
+    fileRead = true;
+};
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+function Read(error, bytesRead, buffer) {
+    if (error) { throw error; }
+    data = buffer.toString("utf8", 0, buf_len);
+}
+
+function Open(err, fd) {
+    if (err) { throw err; }
+    fs.read(fd, buffer, 0, buf_len, null, Read);
+    fs.close(fd, Close);
+}
+
+function Stat(error, sta) {
+    if (error) { throw error; }
+    buffer = new Buffer(sta.size);
+    buf_len = buffer.length;
+    fs.open(fileName, 'r', Open);
+}
+
+function Exists(ext) {
+    if (ext) {
+        fs.stat(fileName, Stat);
+    } else {
+        console.log("No file");
+    }
+}
+
+function Send(request, response) {
+    response.send(data);
+}
+
+fs.exists(fileName, Exists);
+app.get('/', Send);
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
